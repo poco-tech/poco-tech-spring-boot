80d07001fe6e5dd8b00fc25dc72d2911572eb9c5
poco-tech <86156985+poco-tech@users.noreply.github.com>
2021-10-28 11:47:21 +0900

    schema.sql & data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into issues (summary, description) values ('バグA', 'バグがあります');
+insert into issues (summary, description) values ('機能要望B', 'Bに追加機能がほしいです');
+insert into issues (summary, description) values ('画面Cが遅い', '早くしてほしいです');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+create table issues (
+id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+summary VARCHAR(256) NOT NULl,
+description VARCHAR(256) NOT NULl
+);
