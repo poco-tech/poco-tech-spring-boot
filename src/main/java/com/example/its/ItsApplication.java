21a72a476026a47c83fa52b21533d30c867f9ac4
poco-tech <86156985+poco-tech@users.noreply.github.com>
2021-10-28 11:25:25 +0900

    Spring Initializr

---
+++ src/main/java/com/example/its/ItsApplication.java
@@ -0,0 +1,13 @@
+package com.example.its;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItsApplication.class, args);
+	}
+
+}
