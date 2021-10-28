cd02f4dffef1f1126fa119ec40180271b85a1138
poco-tech <86156985+poco-tech@users.noreply.github.com>
2021-10-28 11:43:47 +0900

    Dependency Injection

--- src/main/java/com/example/its/domain/issue/IssueService.java
+++ src/main/java/com/example/its/domain/issue/IssueService.java
@@ -1,7 +1,10 @@
 package com.example.its.domain.issue;
 
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 
+@Service
 public class IssueService {
 
     public List<IssueEntity> findAll() {
--- src/main/java/com/example/its/web/issue/IssueController.java
+++ src/main/java/com/example/its/web/issue/IssueController.java
@@ -1,14 +1,16 @@
 package com.example.its.web.issue;
 
 import com.example.its.domain.issue.IssueService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class IssueController {
 
-    private final IssueService issueService = new IssueService();
+    private final IssueService issueService;
 
     @GetMapping("/issues")
     public String showList(Model model) {
