451b0f160ed99042e6156a0a9f60f659638a17fd
poco-tech <86156985+poco-tech@users.noreply.github.com>
2021-10-28 11:41:29 +0900

    lombok

--- src/main/java/com/example/its/domain/issue/IssueEntity.java
+++ src/main/java/com/example/its/domain/issue/IssueEntity.java
@@ -1,37 +1,12 @@
 package com.example.its.domain.issue;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
 public class IssueEntity {
     private long id;
     private String summary;
     private String description;
-
-    public IssueEntity(long id, String summary, String description) {
-        this.id = id;
-        this.summary = summary;
-        this.description = description;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getSummary() {
-        return summary;
-    }
-
-    public void setSummary(String summary) {
-        this.summary = summary;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
