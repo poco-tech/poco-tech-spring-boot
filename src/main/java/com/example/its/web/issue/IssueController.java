4c72ae8ba4627396a29c58d194a38c0408272e7f
poco-tech <86156985+poco-tech@users.noreply.github.com>
2021-10-28 11:53:58 +0900

    validation

--- src/main/java/com/example/its/web/issue/IssueController.java
+++ src/main/java/com/example/its/web/issue/IssueController.java
@@ -4,6 +4,8 @@ import com.example.its.domain.issue.IssueService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,7 +30,10 @@ public class IssueController {
     }
 
     @PostMapping
-    public String create(IssueForm form, Model model) {
+    public String create(@Validated IssueForm form, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return showCreationForm(form);
+        }
         issueService.create(form.getSummary(), form.getDescription());
         return "redirect:/issues";
     }
--- src/main/java/com/example/its/web/issue/IssueForm.java
+++ src/main/java/com/example/its/web/issue/IssueForm.java
@@ -2,10 +2,18 @@ package com.example.its.web.issue;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Data
 public class IssueForm {
 
+    @NotBlank
+    @Size(max=256)
     private String summary;
+
+    @NotBlank
+    @Size(max=256)
     private String description;
 
 }
