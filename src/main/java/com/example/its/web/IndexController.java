0030b92516bc4d657227aa870668783c5bb49aa1
poco-tech <86156985+poco-tech@users.noreply.github.com>
2021-11-09 21:59:35 +0900

    カスタムログインページを表示しよう

--- src/main/java/com/example/its/web/IndexController.java
+++ src/main/java/com/example/its/web/IndexController.java
@@ -10,4 +10,9 @@ public class IndexController {
     public String index() {
         return "index";
     }
+
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "login";
+    }
 }
