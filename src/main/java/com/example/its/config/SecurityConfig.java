1a1414dd8ade35cc00bdefab2216e44376982543
poco-tech <86156985+poco-tech@users.noreply.github.com>
2021-11-18 22:43:46 +0900

    UserDetailsService を実装しよう

--- src/main/java/com/example/its/config/SecurityConfig.java
+++ src/main/java/com/example/its/config/SecurityConfig.java
@@ -1,11 +1,17 @@
 package com.example.its.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -16,5 +22,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                 .loginPage("/login");
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
     }
 }
---
+++ src/main/java/com/example/its/domain/auth/CustomUserDetailsService.java
@@ -0,0 +1,16 @@
+package com.example.its.domain.auth;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        throw new UsernameNotFoundException(
+          "Given username is not found. (username = '" + username + "')"
+        );
+    }
+}
