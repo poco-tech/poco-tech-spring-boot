e066e01fbb2d80c06201a475d0e93e4b02930ba7
poco-tech <86156985+poco-tech@users.noreply.github.com>
2021-11-19 23:28:36 +0900

    ユーザーの初期データを登録しよう

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
 insert into issues (summary, description) values ('バグA', 'バグがあります');
 insert into issues (summary, description) values ('機能要望B', 'Bに追加機能がほしいです');
 insert into issues (summary, description) values ('画面Cが遅い', '早くしてほしいです');
+
+insert into users (username, password) values ('tom', 'password');
+insert into users (username, password) values ('bob', 'password');
