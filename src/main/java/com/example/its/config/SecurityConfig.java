a84e20b6623bee925ea99ee68e6db8a75142eff5
poco-tech <86156985+poco-tech@users.noreply.github.com>
2021-11-19 23:24:22 +0900

    ユーザーを管理するテーブルを作成しよう

--- src/main/java/com/example/its/config/SecurityConfig.java
+++ src/main/java/com/example/its/config/SecurityConfig.java
@@ -16,6 +16,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        // for h2-console
+        http
+                .authorizeRequests().antMatchers("/h2-console/**").permitAll()
+                .and()
+                .csrf().ignoringAntMatchers("/h2-console/**")
+                .and()
+                .headers().frameOptions().disable();
+
         http
                 .authorizeRequests()
                 .mvcMatchers("/login/**").permitAll()
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,3 +3,8 @@ id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 summary VARCHAR(256) NOT NULl,
 description VARCHAR(256) NOT NULl
 );
+
+create table users (
+username varchar(50) not null primary key,
+password varchar(500) not null
+);
