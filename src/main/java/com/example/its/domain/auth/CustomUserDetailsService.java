b8f0625885e9b20c96d68e9be2a955feaa3cfc41
poco-tech <86156985+poco-tech@users.noreply.github.com>
2021-11-20 21:58:36 +0900

    ユーザー情報をデータベースから取得しよう

--- src/main/java/com/example/its/domain/auth/CustomUserDetailsService.java
+++ src/main/java/com/example/its/domain/auth/CustomUserDetailsService.java
@@ -1,5 +1,6 @@
 package com.example.its.domain.auth;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -8,14 +9,25 @@ import org.springframework.stereotype.Service;
 import java.util.Collections;
 
 @Service
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        if ("tom".equals(username)) {
-            return new CustomUserDetails("tom", "password", Collections.emptyList());
-        }
-        throw new UsernameNotFoundException(
-          "Given username is not found. (username = '" + username + "')"
-        );
+        return userRepository.findByUsername(username)
+                .map(
+                        user -> new CustomUserDetails(
+                                user.getUsername(),
+                                user.getPassword(),
+                                Collections.emptyList()
+                        )
+                )
+                .orElseThrow(
+                        () -> new UsernameNotFoundException(
+                                "Given username is not found. (username = '" + username + "')"
+                        )
+                );
     }
 }
---
+++ src/main/java/com/example/its/domain/auth/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.its.domain.auth;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.Optional;
+
+@Mapper
+public interface UserRepository {
+
+    @Select("select * from users where username = #{username}")
+    Optional<User> findByUsername(String username);
+
+}
