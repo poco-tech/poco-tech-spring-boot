236ba73eefbe69073c8653f85de48d22006fa3e9
poco-tech <86156985+poco-tech@users.noreply.github.com>
2021-11-23 21:58:23 +0900

    ユーザー作成：作成画面を表示するためのハンドラーメソッドを実装しよう

--- src/main/java/com/example/its/web/user/UserController.java
+++ src/main/java/com/example/its/web/user/UserController.java
@@ -7,8 +7,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Collections;
-
 @Controller
 @RequestMapping("/users")
 @RequiredArgsConstructor
@@ -22,4 +20,8 @@ public class UserController {
         return "users/list";
     }
 
+    @GetMapping("/creationForm")
+    public String showCreationForm() {
+        return "users/creationForm";
+    }
 }
