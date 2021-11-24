79d830a89ed6f45cd3f4551296d9c5329b0efaf2
poco-tech <86156985+poco-tech@users.noreply.github.com>
2021-11-24 23:31:49 +0900

    ユーザー作成：画面で入力された値をサーバー側に渡そう

--- src/main/java/com/example/its/web/user/UserController.java
+++ src/main/java/com/example/its/web/user/UserController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -22,12 +23,12 @@ public class UserController {
     }
 
     @GetMapping("/creationForm")
-    public String showCreationForm() {
+    public String showCreationForm(@ModelAttribute UserForm form) {
         return "users/creationForm";
     }
 
     @PostMapping
-    public String create() {
+    public String create(UserForm form) {
         return "redirect:/users";
     }
 }
---
+++ src/main/java/com/example/its/web/user/UserForm.java
@@ -0,0 +1,13 @@
+package com.example.its.web.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UserForm {
+
+    private String username;
+    private String password;
+
+}
