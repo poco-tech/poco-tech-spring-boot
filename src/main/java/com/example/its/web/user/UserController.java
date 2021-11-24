bd8a87d3785824dc0a18a9c57e0da9cbc2a0e3fa
poco-tech <86156985+poco-tech@users.noreply.github.com>
2021-11-24 22:56:09 +0900

    ユーザー作成：ハンドラーメソッドを実装しよう

--- src/main/java/com/example/its/web/user/UserController.java
+++ src/main/java/com/example/its/web/user/UserController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -23,5 +24,10 @@ public class UserController {
     @GetMapping("/creationForm")
     public String showCreationForm() {
         return "users/creationForm";
+    }
+
+    @PostMapping
+    public String create() {
+        return "redirect:/users";
     }
 }
