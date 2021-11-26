f691e0fc7d5db48379879205cd6d81d86dbedc59
poco-tech <86156985+poco-tech@users.noreply.github.com>
2021-11-26 23:50:44 +0900

    ユーザー作成：ユーザー名の重複チェックをする Validator を作ろう (実装途中のためコンパイルエラーあり）

---
+++ src/main/java/com/example/its/web/validation/UniqueUsernameValidator.java
@@ -0,0 +1,18 @@
+package com.example.its.web.validation;
+
+import com.example.its.domain.auth.UserRepository;
+import lombok.RequiredArgsConstructor;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+@RequiredArgsConstructor
+public class UniqueUsernameValidator implements ConstraintValidator<UniqueUsername, String> {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        return userRepository.findByUsername(value).isEmpty();
+    }
+}
