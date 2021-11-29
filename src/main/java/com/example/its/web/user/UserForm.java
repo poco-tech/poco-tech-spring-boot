b6edb0dbb6e3802188b59fce532ce17f8a6d171e
poco-tech <86156985+poco-tech@users.noreply.github.com>
2021-11-29 22:15:59 +0900

    パスワードの長さを確かめるバリデーションを実装しよう

--- src/main/java/com/example/its/web/user/UserForm.java
+++ src/main/java/com/example/its/web/user/UserForm.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Data
 @AllArgsConstructor
@@ -15,6 +16,7 @@ public class UserForm {
     private String username;
 
     @NotBlank
+    @Size(min = 12, max = 128)
     private String password;
 
 }
