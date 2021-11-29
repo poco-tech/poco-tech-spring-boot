526b626157a96ed840f0c3527900a1c30f6dcd15
poco-tech <86156985+poco-tech@users.noreply.github.com>
2021-11-30 00:03:16 +0900

    エンコードしたパスワードを初期データとして投入しよう

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,5 +2,7 @@ insert into issues (summary, description) values ('バグA', 'バグがありま
 insert into issues (summary, description) values ('機能要望B', 'Bに追加機能がほしいです');
 insert into issues (summary, description) values ('画面Cが遅い', '早くしてほしいです');
 
-insert into users (username, password) values ('tom', 'password');
-insert into users (username, password) values ('bob', 'password');
+-- password1234
+insert into users (username, password) values ('tom', '3b977839c7d9fd9256514cb7974f825336ce587940d0252923380a419f4644926c1071bd93d88c28');
+-- password1234
+insert into users (username, password) values ('bob', '1b9415d98c7380dab7d1951742c8354e9d16ed5162ff8ffc88275d7c8a570122f3fc3ba33303f5ac');
