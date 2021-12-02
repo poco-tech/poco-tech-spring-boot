58e32fdd4ae617b6036668784490cb2153065b74
poco-tech <86156985+poco-tech@users.noreply.github.com>
2021-12-02 22:14:16 +0900

    認可③：権限によってWebリソースへの呼び出しを制限しよう

--- src/main/java/com/example/its/config/SecurityConfig.java
+++ src/main/java/com/example/its/config/SecurityConfig.java
@@ -28,6 +28,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                 .mvcMatchers("/login/**").permitAll()
+                .mvcMatchers("/users/**").hasAuthority("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
